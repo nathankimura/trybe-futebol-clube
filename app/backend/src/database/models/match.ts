502b8ae7379da2d4947c8710005e003a04f4808f
Nathan Kimura <nathankimura14@gmail.com>
2022-09-28 18:39:01 -0300

    feat: add requirement 1, create migration and model for user

---
+++ app/backend/src/database/models/match.ts
@@ -0,0 +1,48 @@
+import { INTEGER, Model, BOOLEAN } from 'sequelize';
+import db from '.';
+
+class Match extends Model {
+  public id!: number;
+  public homeTeam!: number;
+  public homeTeamGoals!: number;
+  public awayTeam!: number;
+  public awayTeamGoals!: number;
+  public inProgress!: number;
+}
+
+Match.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'teams',
+  underscored: true,
+  timestamps: false,
+});
+
+export default Match;
---
+++ app/backend/src/database/models/team.ts
@@ -0,0 +1,28 @@
+import { INTEGER, Model, STRING } from 'sequelize';
+import db from '.';
+
+class Team extends Model {
+  public id!: number;
+  public teamName!: string;
+}
+
+Team.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: STRING,
+    allowNull: false,
+    unique: true,
+  },
+}, {
+  sequelize: db,
+  modelName: 'teams',
+  underscored: true,
+  timestamps: false,
+});
+
+export default Team;
